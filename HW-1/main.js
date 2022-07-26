a233ec632200e2d745632bad59b02dd65c0c4b48
Edik-Bernetskyi <bernetskiy200025@gmail.com>
2022-07-26 17:38:08 +0300

    Initial commit

---
+++ HW-1/main.js
@@ -0,0 +1,89 @@
+let hello = 'hello';
+console.log(hello);
+document.write(`${hello} `);
+alert(hello);
+
+let owu = "owu";
+console.log(owu);
+document.write(`${owu} `);
+alert(owu);
+
+let com = 'com';
+console.log(com);
+document.write(`${com} `);
+alert(com);
+
+let ua = "ua";
+console.log(ua);
+document.write(`${ua} `);
+alert(ua);
+
+let number = 1;
+console.log(number);
+document.write(`${number} `);
+alert(number);
+
+let number10 = 10;
+console.log(number10);
+document.write(`${number10} `);
+alert(number10);
+
+let minus999 = -999;
+console.log(minus999);
+document.write(`${minus999} `);
+alert(minus999);
+
+let number123 = 123;
+console.log(number123);
+document.write(`${number123} `);
+alert(number123);
+
+let p = 3.14;
+console.log(p);
+document.write(`${p} `);
+alert(p);
+
+let remainder = 2.7;
+console.log(remainder);
+document.write(`${remainder} `);
+alert(remainder);
+
+let number16 = 16;
+console.log(number16);
+document.write(`${number16} `);
+alert(number16);
+
+let boolean1 = true;
+console.log(boolean1);
+document.write(`${boolean1} `);
+alert(boolean1);
+
+let boolean2 = false;
+console.log(boolean2);
+document.write(`${boolean2} `);
+alert(boolean2);
+
+
+let firstName = `Edward`;
+let middleName = 'Igorovich';
+let lastName = 'Bernetskyi';
+// let res = `${firstName} ${middleName} ${lastName}`;
+// console.log(res);
+let res = firstName + ' ' + middleName + ' ' + lastName;
+console.log(res);
+
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+
+let name = prompt('Enter name');
+let middleName2 = prompt('Enter middleName2');
+let age = +prompt('Enter age');
+console.log(name);
+console.log(middleName2);
+console.log(age);
